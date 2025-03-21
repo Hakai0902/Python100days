27ff34d3f920aa3781a3dbd2eafd9d3e20b1dda5
Hakai <sanketacharya9@gmail.com>
2025-03-21 12:30:19 +0000

    maintained folder structure

---
+++ Day1/day1.py
@@ -0,0 +1,7 @@
+#done yo
+print("welcome to band name generator.")
+print("what's the name of city you grew up in?")
+name1 = input()
+print("what's your pet name?")
+name2 = input()
+print("your band name could be " + name1+ " " + name2)
--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-print("welcome to band name generator.")
-print("what's the name of city you grew up in?")
-name1 = input()
-print("what's your pet name?")
-name2 = input()
-print("your band name could be " + name1+ " " + name2)
+#import Day1.day1
+#day2
